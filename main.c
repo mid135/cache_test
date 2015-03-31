5df6ea9cd802cf6644a2f676061d02782d2e0fc5
mid135 <moskalukdima@mail.ru>
2015-03-31 16:52:29 +0300

    nnn

--- main.c
+++ main.c
@@ -5,8 +5,8 @@
 #include <time.h>
 #include <inttypes.h>
 
-#define STRUCT_SIZE 1
-#define CYCLE_COUNT 50000
+#define STRUCT_SIZE 31
+#define CYCLE_COUNT 50
 
 #define L1_TIME 8  //32K
 #define L2_TIME 15   //256K
@@ -28,10 +28,11 @@ int main(void)
     elem** array=malloc(sizeof(elem)*CYCLE_COUNT);
     //ALLOCATE ELEMENTS AND BIND'EM
     array[0]=(struct elem*)malloc(sizeof(elem));
-    for (uint64_t i=1;i<CYCLE_COUNT;++i){
+    for (uint64_t i=1;i<CYCLE_COUNT-1;++i){
         array[i]=(struct elem*)malloc(sizeof(elem));
         array[i-1]->next=array[i];
     }
+    //array[CYCLE_COUNT-1]->next=array[0];
 
     //RANDOM ALLOCATION
     elem** random_array=malloc(sizeof(elem)*CYCLE_COUNT);
@@ -52,12 +53,7 @@ int main(void)
 
     uint64_t x=0;
 
-    uint64_t avg_1=0;
-    uint64_t hit_1=1;
-    uint64_t avg_2=0;
-    uint64_t hit_2=1;
-    uint64_t avg_3=0;
-    uint64_t hit_3=1;
+    uint64_t avg=0;
 
     uint64_t start;
     uint64_t stop;
@@ -70,15 +66,11 @@ int main(void)
         current=current->next;
 
         stop = _rdtsc();
-        if ((stop-start)<=L1_TIME) {avg_1+=(stop-start);++hit_1;}
-        if ((stop-start)<=L2_TIME) {avg_2+=(stop-start);++hit_2;}
-        if ((stop-start)<=L3_TIME) {avg_3+=(stop-start);++hit_3;}
+        avg+=(stop-start);
     }    
-    printf("conseq access avg time on L1 %" PRIu64 "\n",avg_1/(hit_1));
-    printf("conseq access avg time on L2 %" PRIu64 "\n",avg_2/(hit_2));
-    printf("conseq access avg time on L3 %" PRIu64 "\n",avg_3/(hit_3));
+    printf("conseq access avg time %" PRIu64 "\n",avg/(CYCLE_COUNT));
 
-    uint64_t avg=0;
+    avg=0;
     //RANDOM_ACCESS
     current=random_array[0];
     while(current->next != NULL){
